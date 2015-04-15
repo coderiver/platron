15f419cb2cf02d3c4e4955421f3979b04713f4a9
Валерий <kaktuz34@gmail.com>
2015-04-15 18:36:26 +0300

    init comm

--- js/common.js
+++ js/common.js
@@ -1,19 +1,75 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// order text
+	function order_info(){
+		var text_length = $('.js-order-text').text().length;
+		var parent = $('.js-order-in');
+		var more = $('.js-order-more');
+		if (text_length > 50){
+			more.show();
+		}
+		else {
+			more.hide();
+		}
+		more.on('click', function(){
+			parent.addClass('is-active');
+			return false;
+		});
+		$('.js-order-close').on('click', function(){
+			parent.removeClass('is-active');
+			return false;
+		});
+	}
+	order_info();
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	// tabs
+	function tabs(){
+		var	tabs = ['#card', '#robox', '#purse', '#terminals', '#cashbox', '#from-your-balance', '#internet-banking'],
+			tabList = $('.js-tabs');
+			
+			$('.js-tabs-group').find('li:first a').addClass("is-active");
+		// смена таба
+		function changeTab(){
+			var	tabId = window.location.hash,
+				result = false;
 
-	console.log($('body').html());
+			if (!tabId) window.location.hash = tabs[0];
+			$('.js-tab-link').on('click', function(){
+				$('.js-tab-link').removeClass('is-active');
+				$(this).addClass('is-active');
+			});
+		}changeTab();
+	
+		// проверка хеша
+		function checkHash(){
+			var	tabId = window.location.hash,
+				result = false;
+			// для ИЕ6-8 эмуляция :target
+			function setTarget(obj){
+				$('.js-tabs-cont').removeClass('is-target');
+				$(tabId).addClass('is-target');
+			};
+			// при загрузке страницы проверяем какую вкладку следует открыть	
+			$.each(tabs, function(){
+				if (tabId == this) 
+				{
+					changeTab(tabId);
+					result = true;
+
+					return false;
+				};
+			});
+
+			if (result == false) changeTab();
+		};
+		checkHash();			
+					
+		// отслеживаем изменение хеша
+		$(window).bind('hashchange', function() {
+			checkHash();
+		});
+				
+	}
+	tabs();
+
 });
